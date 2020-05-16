bb7c30baf34fa9afa90d2da617766f88635ed18a
Hiroki Yoshino <hiro.pinot@gmail.com>
2020-05-17 00:42:11 +0900

    Add Pre-Rendering complete

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,44 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import Alert from '../components/alert'
 import utilsStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <Alert type='error'>失敗</Alert>
+      <Alert type='success'>success...!</Alert>
       <section className={utilsStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you_ll be building a site like this on {' '}
           <a href="https://nextjs.org">our Next.js</a>.)
         </p>
+      </section>
+      <section className={`${utilsStyles.headingMd} ${utilsStyles.padding1px}`}>
+        <h2 className={utilsStyles.headingLd}>Blog</h2>
+        <ul className={utilsStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilsStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
